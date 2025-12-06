1a938aed1068ea9e925b47d64c7f3b8898ef141f
Mostafa Elsonbaty <armaggg3@gmail.com>
2025-12-06 18:33:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors(
   {
     origin: [
-      'http://localhost:4200',
+      'http://localhost:4200/login',
       'http://localhost:3000',
       'http://localhost:3001',
     ],
